793f288d95abdb520475187c2b8c73d1a8afba01
Jeff Swisher <jefftswisher@gmail.com>
2019-11-24 00:14:17 +0000

    Returned all data types from api call: team,date,time,venue,city,state,url.

--- lib/nfl_tickets/api.rb
+++ lib/nfl_tickets/api.rb
@@ -1,6 +1,6 @@
 class NflTickets::API
   
-  #attr_accessor :team_name, :date, :time, :venue, :state, :url 
+  attr_accessor :team, :date, :time, :venue, :state, :url, :city
   
 def self.games 
    games = []
@@ -13,11 +13,20 @@ end
   
   
   def self.fetch 
-  
     url = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=m4EQzjgxMQBTg801rcvoUStJnEQ7Emqr&locale=*&countryCode=US&promoterId=705"
     response = HTTParty.get(url)
-    team = response["_embedded"]["events"][0]["name"]
-    team
+    
+    game = self.new
+    game.team = response["_embedded"]["events"][0]["name"]
+    game.url = response["_embedded"]["events"][0]["url"]
+    game.date = response["_embedded"]["events"][0]["dates"]["start"]["localDate"]
+    game.time = response["_embedded"]["events"][0]["dates"]["start"]["localTime"]
+    game.venue = response["_embedded"]["events"][0]["_embedded"]["venues"][0]["name"]
+    game.city = response["_embedded"]["events"][0]["_embedded"]["venues"][0]["city"]["name"]
+    game.state = response["_embedded"]["events"][0]["_embedded"]["venues"][0]["state"]["name"]
+    game
+    binding.pry
   end 
   
+
 end 
