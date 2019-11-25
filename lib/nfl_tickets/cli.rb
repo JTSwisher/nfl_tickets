fe62301ec104efe66b9b2cce01127b4d5d185390
Jeff Swisher <jefftswisher@gmail.com>
2019-11-25 22:01:18 +0000

    Gave user the ability to request infomration on an additional game after first reqeust. Need to adjsut to only return second requested game object and not the previous object as well

--- lib/nfl_tickets/cli.rb
+++ lib/nfl_tickets/cli.rb
@@ -30,20 +30,35 @@ class NflTickets::CLI
   end 
   
   def menu 
-    puts "Would you like to see more infomration on this game(Y/N)?"
-    input = gets.chomp 
+    puts "Would you like to see more information on this game(YES/NO)?"
+    input = gets.chomp
     
-    case input
-    when input == "Yes" || "yes"
-      NflTickets::Games.all.each do |game|
-        puts " Date of game:".colorize(:blue) + " #{game.date}" 
-        puts " Time of game:".colorize(:blue) + " #{game.time}" 
-        puts " Game venue:".colorize(:blue) + " #{game.venue}" 
-        puts " Game location:\n".colorize(:blue) + "    City: ".colorize(:light_blue) + "#{game.city}\n" + "    State: ".colorize(:light_blue) + "#{game.state}" 
-        puts " Tickets for purchase:".colorize(:blue) + " #{game.url}"
-      end 
-    when input == "No" || "no"
-      call 
+    case input.upcase
+      when  "YES"
+        NflTickets::Games.all.each do |game|
+          puts " Date of game:".colorize(:blue) + " #{game.date}" 
+          puts " Time of game:".colorize(:blue) + " #{game.time}" 
+          puts " Game venue:".colorize(:blue) + " #{game.venue}" 
+          puts " Game location:\n".colorize(:blue) + "    City: ".colorize(:light_blue) + "#{game.city}\n" + "    State: ".colorize(:light_blue) + "#{game.state}" 
+          puts " Tickets for purchase:".colorize(:blue) + " #{game.url}"
+        end 
+      when "NO"
+        call 
+    end 
+    
+    puts "Would you like information on a different game(YES/NO)?"
+    input = gets.chomp
+    
+    case input.upcase
+      when "YES"
+        call
+      when "NO"
+        exit
+      end
     end 
   end 
-end 
+  
+  
+  
+  
+  
